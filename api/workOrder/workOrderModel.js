a6d914a0a1bab80a38c966f7364b604bda57b79b
Matt Taylor <matt@mtaylor.tech>
2021-01-01 20:37:31 -0500

    add 'findAllRelated' to workOrderModel to prepare for future features

--- api/workOrder/workOrderModel.js
+++ api/workOrder/workOrderModel.js
@@ -108,6 +108,45 @@ const findById = async (workOrderId) => {
     .first();
 };
 
+const findAllRelated = (userId, companyIds, propertyIds) => {
+  return db('workOrders')
+    .from('workOrders as wo')
+    .where(function () {
+      this.where('createdBy', userId)
+        .orWhere('assignedTo', userId)
+        .orWhereIn('company', companyIds)
+        .orWhereIn('property', propertyIds);
+    })
+    .join('profiles as u', 'wo.createdBy', 'u.id')
+    .join('profiles as a', 'wo.assignedTo', 'a.id')
+    .join('companies as c', 'c.id', 'wo.company')
+    .join('properties as p', 'p.id', 'wo.property')
+    .join('priority as pr', 'pr.id', 'wo.priority')
+    .join('status as s', 's.id', 'wo.status')
+    .join('comments', 'wo.id', 'comments.workOrder')
+    .join('images', 'wo.id', 'images.workOrder')
+    .select(
+      'wo.id',
+      'wo.title',
+      'wo.description',
+      'wo.created_at',
+      'wo.updated_at',
+      db.raw('row_to_json(s) as status'),
+      db.raw('row_to_json(pr) as priority'),
+      db.raw('row_to_json(c) as company'),
+      db.raw('row_to_json(p) as property'),
+      db.raw(
+        'ARRAY(SELECT row_to_json(images) FROM images WHERE images."workOrder" = wo.id) AS images'
+      ),
+      db.raw(
+        'ARRAY(SELECT row_to_json(comments) FROM comments WHERE comments."workOrder" = wo.id) AS comments'
+      ),
+      db.raw('row_to_json(u) as createdBy'),
+      db.raw('row_to_json(a) as assignedTo')
+    )
+    .distinctOn('wo.id');
+};
+
 const create = async (workOrder) => {
   return db('workOrders').insert(workOrder).returning('*');
 };
@@ -129,6 +168,7 @@ module.exports = {
   findByUser,
   findBy,
   findById,
+  findAllRelated,
   create,
   update,
   remove,
